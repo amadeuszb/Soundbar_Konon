65969c66b56bc9c7d2079f1f49f32db504fb476a
Amadeusz Bubniak <bubniak.amadeusz@gmail.com>
2017-12-04 22:25:17 +0100

    first commit

---
+++ app/src/main/java/amadeuszbubniak/kononowicz_soundboard/SoundboardActivity.java
@@ -0,0 +1,13 @@
+package amadeuszbubniak.kononowicz_soundboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundboard);
+    }
+}
